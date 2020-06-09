d0d4bcc6429ca274c492c17d3697d783090fdec6
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-09 22:47:13 +0100

    Express initialised, port created and listened to

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require('path');
 const fs = require("fs");
 
+// Variable to hold instance of express object
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Variable to hold local port or port of deployed site
 
 const PORT = process.env.PORT || 8080;
+
+// Listening to port and console logging confirmation of port being listened to
 
 app.listen(PORT, () => console.log (`Server started on port ${PORT}`));
