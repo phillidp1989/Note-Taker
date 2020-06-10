3bd41f44a072d19df240ba71385710047ef5f0d8
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-10 16:09:02 +0100

    HTML and API routes have been setup

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const path = require('path');
 const fs = require("fs");
+const util = require("util");
+
+const jsonFolderPath = path.join(__dirname, "db");
+const jsonFilePath = path.join(jsonFolderPath, "/db.json");
+
+
 
 // Variable to hold instance of Express object
 
@@ -17,17 +23,26 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route handling for html
+// HTML route handling
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public", "notes.html"));
 });
 
+// API route handling
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile(jsonFilePath, (err, data) => {  
+        if (err) throw err;      
+        res.json(JSON.parse(data))
+    })
+})
+
+// Final HTML routes to direct users to the homepage
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public", "index.html"));
 });
-
-
 
 // Listening to port and console logging confirmation of port being listened to
 
