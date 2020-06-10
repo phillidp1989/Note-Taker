c5d772f7ffa385b72e4897abc79e261ce0a5f33c
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-10 11:26:57 +0100

    html routes setup for index and notes.html files

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ const express = require("express");
 const path = require('path');
 const fs = require("fs");
 
-// Variable to hold instance of express object
+// Variable to hold instance of Express object
 
 const app = express();
 
 // Variable to hold local port or port of deployed site
 
 const PORT = process.env.PORT || 8080;
+
+// Setup Express to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route handling for html
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public", "notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public", "index.html"));
+});
+
+
 
 // Listening to port and console logging confirmation of port being listened to
 
